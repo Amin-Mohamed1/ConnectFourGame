ab7a98b80ab105dc1004f07bc0318958ae84d624
Omar-Eldawy <eng.omar.aldawy@gmail.com>
2024-11-28 16:40:17 +0200

    add max depth to solver interface

--- BackEnd/Services/Solver.py
+++ BackEnd/Services/Solver.py
@@ -4,5 +4,5 @@ from abc import abstractmethod
 class Solver:
     @staticmethod
     @abstractmethod
-    def solve(board: list[list[str]], piece: str) -> int:
+    def solve(board: list[list[str]], piece: str, max_depth: int) -> int:
         pass
